11679843a728194bd258b77c5fdc43ca5b866d04
Fernando <ferweb.reyna@gmail.com>
2022-09-13 00:54:56 -0300

    Desafios

---
+++ miapp/src/App.js
@@ -0,0 +1,31 @@
+import './estilos.css';
+import NavbarBurguer from './assets/NavbarBurguer';
+import Destacados from './assets/Destacados';
+import Cupones from './assets/Cupones';
+import PromosApp from './assets/PromosApp';
+import Footer from './assets/Footer';
+import { NavBar } from './components/NavBar/NavBar';
+import  {ItemListContainers}  from './ItemListContainers';
+import CartWidget from './components/CartWidget/CartWidget';
+
+function App() {
+  return (
+    <div className='container-fluid fondo_amarillo App'>
+      <NavBar/>
+      <CartWidget/>
+      <NavbarBurguer />
+      <hr />
+      <ItemListContainers greeting = 'Bienvenidos a BuyAndHold' />
+      <Destacados/>
+      <Cupones/>
+      <PromosApp/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
+
+
+//COMPONENTS --- CONTAINERS
+
